2392ed5a6b8367e73e0a8b1323243cfb55f35861
facundo <facusoto1998@gmail.com>
2021-08-13 11:27:22 -0300

    que idiota

--- figuras.js
+++ figuras.js
@@ -1,3 +1,4 @@
+// Código del cuadrado
 const ladoCuadrado = 5;
 console.log("Los lados del cuadrado miden: " + ladoCuadrado+ "cm");
 
@@ -6,3 +7,13 @@ console.log("El perimetro del cuadrado mide: " + perimetroCuadrado+ "cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El área del cuadrado mide: " + areaCuadrado + "cm²");
+
+// Código del triangulo
+const ladoTriangulo1 = 6;
+console.log("El lado 1 del triangulo mide: " + ladoCuadrado+ "cm");
+
+const ladoTriangulo2 = 6;
+console.log("El lado 2 del triangulo mide: " + ladoCuadrado+ "cm");
+
+const ladoTriangulo3 = 4;
+console.log("El lado 3 del triangulo mide: " + ladoCuadrado+ "cm");
