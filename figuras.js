31435f3cb89a97ba6ede7a667e8e36a7abe5022d
facundo <facusoto1998@gmail.com>
2021-08-13 11:26:07 -0300

    translado pc

---
+++ figuras.js
@@ -0,0 +1,8 @@
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado+ "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado mide: " + perimetroCuadrado+ "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado mide: " + areaCuadrado + "cm²");
